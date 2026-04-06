d16e4905a000b36da53f44ada152b94f1d9cad5f
cfe <cfe@glitchlesslabs.io>
2026-04-06 11:45:21 +0800

    chore: add the cashflow card and dynamic loading data

--- app/dashboard/cashflow.tsx
+++ app/dashboard/cashflow.tsx
@@ -1,6 +1,22 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { getAnnualCashflow } from '@/data/getAnnualCashflow';
+import CashflowFilters from './cashflow-filters';
+import { getTransactionYearsRange } from '@/data/getTransactionYearsRange';
 
-export default async function Cashflow() {
-  const cashflow = await getAnnualCashflow(2026);
-  return <div></div>;
+export default async function Cashflow({ year }: { year: number }) {
+  const [cashflow, yearsRange] = await Promise.all([
+    getAnnualCashflow(year),
+    getTransactionYearsRange(),
+  ]);
+
+  return (
+    <Card className='mb-5'>
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          <span>Cashflow</span>
+          <CashflowFilters year={year} yearsRange={yearsRange} />
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  );
 }
